beaee1c47c24c747cf42341717eecbb24c3ee904
enelsotillo <enelsotillo@gmail.com>
2022-01-22 08:45:31 -0300

    2.0 commit

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+numero1=0
+numero2=0
+resultado=0
+print("BIENVENIDO AL SISTAMA PARA CALCULAR TABLAS DE MULTIPLICAR")
+numero1=input("ingrese numero ")
+for(numero2=1 numero2<=10 numero2++)
+    resultado=numero1*numero2
+print(numero1+" X "+numero2+" = "+resultado)
